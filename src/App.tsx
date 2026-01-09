046eb8da8269761490ede7a6ba8dba4d97299328
Jaime Convery <jaimeconvery@gmail.com>
2026-01-09 10:15:53 -0500

    feat: install typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./reset.css";
+import "./App.css";
+import Router from "./components/Router";
+
+function App() {
+    return (
+        <div className="container">
+            <Router />
+        </div>
+    );
+}
+
+export default App;
